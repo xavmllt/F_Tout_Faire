36239caab45fcc2bdcaf37a9bc78f3a2ec79afe9
xavmllt <xaviermaillot100@gmail.com>
2024-02-12 10:53:01 +0100

    Modification dans le style pour bien aligné les éléments du footer

---
+++ test.js
@@ -0,0 +1,8 @@
+const grille =[0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+for(i = 0; i < grille.length; i++){
+    if(grille[i] === 0){
+        document.getElementById("concaténation").style.backgroundColor = "red"
+    }else{
+        document.getElementById("concaténation").style.backgroundColor = "blue";
+    };
+};
